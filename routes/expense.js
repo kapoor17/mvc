3646d03f01349be21d87697e21eb39bea352ffa8
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-06-07 11:47:37 +0530

    update an expense

--- routes/expense.js
+++ routes/expense.js
@@ -4,22 +4,24 @@ const express = require('express');
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
-const {create, expenseById, read} = require('../controllers')
+const {create, expenseById, read, update} = require('../controllers')
 
 // Create POST route to create an expense
 router.post(("/expenses/create", create));
 
 // Create PUT route to update an expense
+router.put("/expenses/:id", expenseById, update)
+
+
+// Create DELETE route to remove an expense
+
 /**
+ * Create GET route to read an expense
  * expensesById, the middleware function get the req and res and queries the
  * database to get the data, and then sends that data to the read function using
  * a next params sent in the middleware function.
  */
 router.get("/expense/:id", expenseById, read);
-
-// Create DELETE route to remove an expense
-
-// Create GET route to read an expense
 
 // Create GET route to read a list of expenses
 
